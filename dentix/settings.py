65085f55ac5cc02e09794bb6cdf0e5b0a0ab4274
walter brunetti <walterbrunetti@gmail.com>
2015-06-01 18:23:02 -0300

    aasdas

--- dentix/settings.py
+++ dentix/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 DEFAULT_APPS = (
+    'bootstrap_admin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
